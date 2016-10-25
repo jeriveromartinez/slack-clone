a92d53436a4caa6e7c1fc2785d63c6abce3c3440
Julio Rivero <jeriveromartinez@gmail.com>
2016-10-25 09:06:42 -0400

    try create form user-profile

--- system_account/forms.py
+++ system_account/forms.py
@@ -1,12 +1,31 @@
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+
+from plataforma.models import Profile
+from django.forms import ModelForm, fields_for_model, model_to_dict
 
 
-class UpdateUserForm(ModelForm):
+class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ('last_name', 'first_name', 'email')
+        fields = ('first_name', 'last_name', 'email')
 
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request', None)
-        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+class ProfileForm(ModelForm):
+    def __init__(self, instance=None, *args, **kwargs):
+        _fields = ('first_name', 'last_name', 'email')
+        _initial = model_to_dict(instance.user, _fields) if instance is not None else {}
+        super(ProfileForm, self).__init__(initial=_initial, instance=instance, *args, **kwargs)
+        self.fields.update(fields_for_model(User, _fields))
+
+    class Meta:
+        model = Profile
+        exclude = ('user', 'company', 'type')
+
+    def save(self, *args, **kwargs):
+        u = self.instance.user
+        u.first_name = self.cleaned_data['first_name']
+        u.last_name = self.cleaned_data['last_name']
+        u.email = self.cleaned_data['email']
+        u.save()
+        profile = super(ProfileForm, self).save(*args, **kwargs)
+        return profile
--- system_account/views.py
+++ system_account/views.py
@@ -7,7 +7,8 @@ from django.template import RequestContext
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_protect
 
-from system_account.forms import UpdateUserForm
+from plataforma.models import Profile
+from system_account.forms import ProfileForm, UserForm
 
 
 @login_required(login_url='/login/')
@@ -30,16 +31,16 @@ def setting_profile(request):
 def setting_profile_edit(request):
     args = {}
     args.update(csrf(request))
-    user = get_object_or_404(User, username=request.user.username)
-    if request.method == 'POST':
-        form = UpdateUserForm(request.POST or None, instance=user)
+    if request.method == "POST":
+        user = get_object_or_404(User, username=request.user)
+        # form_user = UserForm(request.POST, instance=request.user)
+        form = ProfileForm(request.POST or None, instance=user)
         if form.is_valid():
             form.save()
             return redirect('account:profile')
         else:
             args['form'] = form
     else:
-        form = UpdateUserForm(instance=request.user)
-
+        form = ProfileForm(instance=request.user.profile)
     return render_to_response('account/acc_edit_profile.html', {'form': form},
                               context_instance=RequestContext(request))
