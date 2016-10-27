96d91e6cfecd482b6510d13988d359daa167f1d8
Julio Rivero <jeriveromartinez@gmail.com>
2016-10-27 09:51:54 -0400

    redefine modules

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,14 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.sessions.models import Session
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from django.utils import timezone
 from django.views.decorators.csrf import csrf_protect
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 from plataforma.forms import LoginForm
 from plataforma.forms import MyRegistrationForm
@@ -82,35 +78,4 @@ def find_team(request):
     return render_to_response('find_team.html', context_instance=RequestContext(request))
 
 
-@api_view(['GET'])
-def room_by_company(request, company):
-    room = Room.objects.all().filter(company__slug=company)
-    serializer = RoomSerializer(room, many=True)
-    return Response(serializer.data)
 
-
-@api_view(['GET'])
-def profile_by_username(request, username):
-    room = Profile.objects.all().filter(user__username=username)
-    serializer = ProfileSerializer(room, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def users_by_company(request, company):
-    profiles = Profile.objects.all().filter(company__slug=company)
-    serializer = ProfileSerializer(profiles, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def users_logged(request, company):
-    sessions = Session.objects.filter(expire_date__gte=timezone.now())
-    uid_list = []
-    for session in sessions:
-        data = session.get_decoded()
-        uid_list.append(data.get('_auth_user_id', None))
-
-    profile = Profile.objects.filter(user__pk__in=uid_list).filter(company__slug=company)
-    serializer = ProfileSerializer(profile, many=True)
-    return Response(serializer.data)
---
+++ rest_service/views.py
@@ -0,0 +1,41 @@
+from django.utils import timezone
+from plataforma.serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.sessions.models import Session
+
+
+# Create your views here.
+
+@api_view(['GET'])
+def room_by_company(request, company):
+    room = Room.objects.all().filter(company__slug=company)
+    serializer = RoomSerializer(room, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def profile_by_username(request, username):
+    room = Profile.objects.all().filter(user__username=username)
+    serializer = ProfileSerializer(room, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def users_by_company(request, company):
+    profiles = Profile.objects.all().filter(company__slug=company)
+    serializer = ProfileSerializer(profiles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def users_logged(request, company):
+    sessions = Session.objects.filter(expire_date__gte=timezone.now())
+    uid_list = []
+    for session in sessions:
+        data = session.get_decoded()
+        uid_list.append(data.get('_auth_user_id', None))
+
+    profile = Profile.objects.filter(user__pk__in=uid_list).filter(company__slug=company)
+    serializer = ProfileSerializer(profile, many=True)
+    return Response(serializer.data)
