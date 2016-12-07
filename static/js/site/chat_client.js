ddabcca2942eac8109c93f22e6acf08b21566522
vbuilvicente <vbuilvicente@gmail.com>
2016-12-07 15:21:05 -0500

    call ui modals

--- static/js/site/chat_client.js
+++ static/js/site/chat_client.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-    var socket = new io.Socket(document.domain, {reconnection: true});
+    var socket = new io.Socket(document.domain, {reconnection: true, secure: true});
 
 
     socket.connect();
