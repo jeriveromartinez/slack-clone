f3df6331d0a5d1d70c00a3b1f4093bef68f0418d
vbuilvicente <vbuilvicente@gmail.com>
2017-02-01 10:18:09 -0500

    resolve merge conflit

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -53,21 +53,14 @@ MANAGERS = ADMINS
 # }
 DATABASES = {
     'default': {
-<<<<<<< HEAD
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'plataforma',
-        'USER': 'root',
-        'PASSWORD': 'S0l0y0lol',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-=======
+
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd5do51m0qg0sdl',
         'USER': 'dfzisdazemrtoc',
         'PASSWORD': 'da85d25b1279f71643cdf29de58a64802f28243566f3d2b3dd44cb520f3a6459',
         'HOST': 'ec2-54-235-72-121.compute-1.amazonaws.com',
         'PORT': '5432',
->>>>>>> cd29479805bf2cfa5c712acf8dae066fcfe1b793
+
     }
 }
 # DATABASES = {
@@ -321,9 +314,9 @@ SOCKETIO_PORT = 8000
 
 USER_AGENTS_CACHE = 'default'
 
-#CORS_ORIGIN_WHITELIST = (
+# CORS_ORIGIN_WHITELIST = (
 #    '0.0.0.0:8000'
 #    'django',
-#)
+# )
 
 CORS_URLS_REGEX = r'^/api/.*$'
