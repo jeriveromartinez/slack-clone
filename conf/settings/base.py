bf0ed82970590ca9c9b7f1a42a8811bfa08adcdb
Julio Rivero <jeriveromartinez@gmail.com>
2017-02-09 00:56:23 -0500

    conection refused

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -58,7 +58,7 @@ DATABASES = {
         'NAME': 'dc49at5g9oe0e6',
         'USER': 'ldtwmxhztkeagd',
         'PASSWORD': 'b1cf46fb4e46b15d376a230b0e79abdb10f43708cb601148f70029a76d5fc608',
-        'HOST': 'ec2-54-235-72-121.compute-1.amazonaws.com',
+        'HOST': 'ec2-54-235-247-224.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
